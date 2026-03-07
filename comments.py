1a6f437147e937a907cd73557cab645ae07bbacf
Roopa-kr <nisharoopa3@gmail.com>
2026-03-07 21:59:52 +0530

    know how comment works

---
+++ comments.py
@@ -0,0 +1,28 @@
+# Comments can be used to explain Python code.
+
+# Comments can be used to make the code more readable.
+
+# Comments can be used to prevent execution when testing code.
+
+# Creating a Comment
+# Comments starts with a #, and Python will ignore them:
+
+
+#This is a comment
+print("Hello, World!")
+# You can also make a comment out of a whole line or a part of a line:
+print("Hello, World!") # This is a comment
+# Comments are for humans to read, and they are ignored by the Python interpreter. They are a great way to explain what your code does, or to leave notes for yourself or other programmers who may read your code in the future.
+# You can also use comments to temporarily disable code:
+# print("This line will not be executed.")
+# In the example above, the print statement is commented out, so it will not be executed when you run the program. This can be useful for testing or debugging your code without deleting it.
+
+#Multi-line Comments
+# If you want to write a comment that spans multiple lines, you can use triple quotes (
+# ''' or """) to create a multi-line comment:
+'''
+This is a multi-line comment.
+It can span multiple lines.
+'''
+# In the example above, the text between the triple quotes is a multi-line comment. It can be used to provide more detailed explanations or to comment out larger sections of code.
+
