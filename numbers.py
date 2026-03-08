74e5637462634cbc4f8e3b8a37f28dc6d0cfc509
Roopa-kr <nisharoopa3@gmail.com>
2026-03-08 23:05:53 +0530

    Numbers

---
+++ numbers.py
@@ -0,0 +1,19 @@
+#Numbers in python 
+#In Python, there are several types of numbers that you can use to represent different kinds of numerical data. The main types of numbers in Python include:
+# 1. Integer (int): Whole numbers without a decimal point. For example:
+x = 10
+print(type(x))  # Output: <class 'int'>
+# 2. Float (float): Numbers with a decimal point. For example:
+y = 3.14
+print(type(y))  # Output: <class 'float'>   
+# 3. Complex (complex): Numbers that have a real part and an imaginary part. For example:
+z = 2 + 3j
+print(type(z))  # Output: <class 'complex'>
+# You can perform various operations on numbers in Python, such as addition, subtraction, multiplication, division, and more. For example:
+a = 5
+b = 3
+c = a + b   
+print(c)  # Output: 8
+d = a - b   
+print(d)  # Output: 2
+# et cetera
