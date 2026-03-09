c2f0cd8d18807c4beee31349f6defed0bb9e338e
Roopa-kr <nisharoopa3@gmail.com>
2026-03-10 00:07:05 +0530

    Booleans in python

---
+++ booleans.py
@@ -0,0 +1,11 @@
+#booleans in python
+#boolean is a data type that can only have two values: True or False
+
+#creating boolean variables
+is_student = True
+is_adult = False
+
+#boolean operations
+print(is_student and is_adult)  # Output: False
+print(is_student or is_adult)   # Output: True
+print(not is_student)           # Output: False 
