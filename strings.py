0fbffd3ff547245a1c6c80ae6b40add9d342ab70
Roopa-kr <nisharoopa3@gmail.com>
2026-03-09 23:28:16 +0530

    Strings in python

---
+++ strings.py
@@ -0,0 +1,11 @@
+#strings in python
+#string is a sequence of characters
+
+#creating a string
+name = "John"
+#string concatenation
+first_name = "John"
+last_name = "Doe"
+full_name = first_name + " " + last_name
+print(full_name)  # Output: "John Doe"
+
