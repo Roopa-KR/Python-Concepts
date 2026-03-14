095083a6b7c9716957c7a8bad0ebdd83c6f6cf59
Roopa-kr <nisharoopa3@gmail.com>
2026-03-14 22:39:35 +0530

    Assignment operator in python

---
+++ assignmentoperator.py
@@ -0,0 +1,16 @@
+#Assignment operator is used to assign a value to a variable. It is represented by the symbol "=". The assignment operator can be used in various ways, such as:
+
+# Simple assignment
+x = 10
+print(x)  # Output: 10
+# Augmented assignment
+x += 5  # Equivalent to x = x + 5
+print(x)  # Output: 15
+x *= 2  # Equivalent to x = x * 2
+print(x)  # Output: 30
+x /= 3  # Equivalent to x = x / 3
+print(x)  # Output: 10.0
+x -= 2  # Equivalent to x = x - 2
+print(x)  # Output: 8.0 
+x //= 2  # Equivalent to x = x // 2
+print(x)  # Output: 4.0 
