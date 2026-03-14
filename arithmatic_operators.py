4d5af5ccbff449e916637f6abe8da68a16a80e95
Roopa-kr <nisharoopa3@gmail.com>
2026-03-14 22:35:22 +0530

    Arithmatic operators in python

---
+++ arithmatic_operators.py
@@ -0,0 +1,18 @@
+# arithmatic operators in python
+# arithmatic operators are used to perform mathematical operations on numbers
+# addition
+a = 10
+b = 5
+print(a + b)  # Output: 15
+# subtraction
+print(a - b)  # Output: 5
+# multiplication
+print(a * b)  # Output: 50
+# division
+print(a / b)  # Output: 2.0
+# floor division
+print(a // b)  # Output: 2
+# modulus
+print(a % b)  # Output: 0
+# exponentiation
+print(a ** b)  # Output: 100000
