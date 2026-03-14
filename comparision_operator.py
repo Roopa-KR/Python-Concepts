2941f27749174a931e790881343ca2c9f031b6a5
Roopa-kr <nisharoopa3@gmail.com>
2026-03-14 22:41:56 +0530

    Comparision operator

---
+++ comparision_operator.py
@@ -0,0 +1,16 @@
+#Comparison operators in python
+#Comparison operators are used to compare two values
+#Equal
+a = 10
+b = 5
+print(a == b)  # Output: False
+#Not equal
+print(a != b)  # Output: True
+#Greater than
+print(a > b)  # Output: True
+#Less than
+print(a < b)  # Output: False
+#Greater than or equal to
+print(a >= b)  # Output: True
+#Less than or equal to
+print(a <= b)  # Output: False
