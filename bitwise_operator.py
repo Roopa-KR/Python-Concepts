baf86cd909027df0e5644408d9cbd56dc48fc68e
Roopa-kr <nisharoopa3@gmail.com>
2026-03-14 22:43:23 +0530

    Bitwise operators in python

---
+++ bitwise_operator.py
@@ -0,0 +1,22 @@
+# Bitwise operators are used to perform bitwise operations on integers.
+# Bitwise operators include:
+# AND (&)
+# OR (|)
+# XOR (^)
+# NOT (~)   
+# Left Shift (<<)
+# Right Shift (>>)
+# Bitwise AND
+a = 5  # In binary: 0101
+b = 3  # In binary: 0011
+print(a & b)  # Output: 1 (In binary: 0001)
+# Bitwise OR
+print(a | b)  # Output: 7 (In binary: 0111)
+# Bitwise XOR
+print(a ^ b)  # Output: 6 (In binary: 0110)
+# Bitwise NOT
+print(~a)  # Output: -6 (In binary: ...11111010)
+# Left Shift
+print(a << 1)  # Output: 10 (In binary: 1010)
+# Right Shift
+print(a >> 1)  # Output: 2 (In binary: 0010)
