5af4219e5e4eda4eba97674efc0ca8c77993e056
Roopa-kr <nisharoopa3@gmail.com>
2026-03-16 23:16:29 +0530

    SETS IN PYTHON

---
+++ set.py
@@ -0,0 +1,9 @@
+#sets in python
+# sets are unordered collection of unique items
+# sets are mutable, but the items in the set must be immutable
+# sets are defined using curly braces {} or the set() function
+# creating a empty set
+my_set = set()
+print(my_set)  # Output: set()
+# creating a set with some elements
+my_set = {1, 2, 3, 4, 5}
